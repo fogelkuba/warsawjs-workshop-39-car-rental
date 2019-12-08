06c25e98229a4419a11f0c14d2e27db1e82af839
fogelkuba <fogelkuba@gmail.com>
2019-12-08 12:52:06 +0100

    dependency injection => remover require form orutes controllers

--- src/routes/index.get.js
+++ src/routes/index.get.js
@@ -1,7 +1,18 @@
 'use strict';
 
-module.exports = function(app) {
+// const db = require('../db');
+// const { cars } = db.transaction(async function(transaction) {
+//   const car = await transaction('cars')
+//
+//   return { cars };
+// });
+
+const cars = ['Honda', 'Ford', 'Toyota'];
+
+
+module.exports = function(app, { db }) {
   app.get('/', function(request, response) {
-    response.view('index', {});
+    console.log(cars)
+    response.view('index', {cars: cars});
   });
 };
